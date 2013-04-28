567fefbe9ec9ac2e2b1dc3c7ad3b4733654eb179
Jon Cundill <jcundill@gmail.com>
2013-04-28 23:09:21 +0100

    fixed comment

--- src/main/scala/jon/test/JsonParser.scala
+++ src/main/scala/jon/test/JsonParser.scala
@@ -39,7 +39,7 @@ object JsonParser {
 }
 
 /**
- * this class understands how to get the info we need to tell gource about all of the various calls into the json api provided by hudson
+ * this class understands how to get the info we need to tell gource about from all of the various calls into the json api provided by hudson
  */
 class JsonParser(config: Config) {
 
